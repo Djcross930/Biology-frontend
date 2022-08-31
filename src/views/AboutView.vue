e3655a4eff390b320d2d58c23023da03ed06fcef
Dan Cross <dancross96@gmail.com>
2022-08-31 18:12:02 -0500

    added new hidden text from a button, and started the sequence viewer

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,41 @@
+<script>
+export default {
+  data: function () {
+    return {
+      message: "",
+      value1: true,
+      value2: false
+    };
+  },
+  created: function () {
+  },
+  watch: {
+    value() {
+      console.log("yo")
+      this.showHidden()
+    }
+  },
+  methods: {
+    showHidden: function () {
+      if (this.value2 === false) {
+        this.value2 = true
+      } else {
+        this.value2 = false
+      }
+    }
+  },
+};
+</script>
 <template>
   <div class="about">
     <h1>The NCBI database has information on every single gene in our bodies, down to the base pair level.</h1>
     <h2>(Don't worry, there still some variation that inevitably sets you apart)</h2>
+    <p> Humans have 22 chromosome pairs, but if you count the two individual sex chromosomes, it comes out to 24 total.
+    </p>
+    <p> Select a chromosome from the list below to get a closer look at what makes you, <b>you.</b></p>
     Select a chromosome from the list:<br />
 
-    <select onchange="loadSV(event.target.value);">
+    <select onchange="loadSV(event.target.value)">
       <option value="">-</option>
       <option value="NC_000001">Chromosome 1</option>
       <option value="NC_000002">Chromosome 2</option>
@@ -35,5 +66,7 @@
 
     <div id="mySeqViewer1">
     </div>
+    <button v-on:click="showHidden()">First Time using the Chromosome Selector?</button>
+    <div v-show="value2">I'm not rendered!</div>
   </div>
 </template>
---
+++ src/views/SeqView.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+    };
+  },
+  created: function () {
+
+  },
+  methods: {},
+};
+</script>
+  
+<template>
+  <h1> YO </h1>
+</template>
