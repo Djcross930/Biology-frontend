c7b82b8e04d2fe06e937618bdaa0854635ec398f
Dan Cross <dancross96@gmail.com>
2022-09-02 13:11:49 -0500

    added new axios request to backend for searching new genes

--- src/views/SeqView.vue
+++ src/views/SeqView.vue
@@ -3,15 +3,29 @@ import axios from "axios";
 export default {
   data: function () {
     return {
+      gene: "",
+      geneId: "",
+      sequence: {}
     };
   },
   created: function () {
 
   },
-  methods: {},
+  methods: {
+    searchGene: function () {
+      axios.post("/genes", { common_name: this.gene }).then(response => {
+        console.log(response.data)
+      })
+    }
+  }
 };
 </script>
   
 <template>
   <h1> YO </h1>
+  <p>{{ gene }}</p>
+  <input type="text" v-model="this.gene"> <button v-on:click="searchGene()">Search</button>
+  <div>
+    <p> Temporary </p>
+  </div>
 </template>
