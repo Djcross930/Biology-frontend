e4cffae33dda7df05442a7bb0821103180f3e9ce
Dan Cross <dancross96@gmail.com>
2022-09-09 14:02:10 -0500

    added text boxes that display the results on the original sequence viewer page, will do the show page next

--- src/views/SeqView.vue
+++ src/views/SeqView.vue
@@ -4,7 +4,8 @@ export default {
   data: function () {
     return {
       gene: "",
-      geneInfo: {}
+      geneInfo: {},
+      geneSequence: {}
     };
   },
   created: function () {
@@ -28,15 +29,18 @@ export default {
 </script>
   
 <template>
-  <h1> Gene Explorer </h1>
+  <h1> Sequence Explorer </h1>
   <p>Search a gene using it's common name.</p>
   <input type="text" v-model="this.gene"> <button v-on:click="searchGene()">Search</button>
   <div id="myDIV" style="display:none">
     <h1>{{ this.geneInfo.common_name }}</h1>
-    <p> <b>Amino Acid Sequence: 5'→3' Direction</b></p>
-    <p> {{ this.geneInfo.amino_acid_sequence }} </p>
-    <p> <b>Original Coding Sequence</b></p>
-    <p> {{ this.geneInfo.cds_sequence }} </p>
+    <h1> <b>Amino Acid Sequence: 5'→3' Direction</b></h1>
+    <!-- <p> {{ this.geneInfo.amino_acid_sequence }} </p> -->
+    <!-- <input type="text" v-model="this.geneInfo.amino_acid_sequence" style="height:120px; width:200px;"> -->
+    <textarea name="Text1" cols="110" rows="4">{{ this.geneInfo.amino_acid_sequence }}</textarea>
+    <h1> <b>Original Coding Sequence</b></h1>
+    <textarea name="Text1" cols="110" rows="9">{{ this.geneInfo.cds_sequence }}</textarea>
+    <br />
     <a v-bind:href="`/genes/${geneInfo.id}`"> See More </a>
   </div>
 </template>
